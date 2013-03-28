61e9d0f6a5fa83175b157fa2eba3e658917124a6
Gregory Nicholas <faction.gregory@gmail.com>
2013-03-28 08:14:32 +0000

    refactored to be much simpler.

--- gae_console.py
+++ gae_console.py
@@ -15,48 +15,59 @@
 import sys
 from os import path
 from os import environ
-from google.appengine.ext.remote_api import remote_api_stub
 
-__all__ = ['connect_to_app', 'config_console', 'config_history', 'enable_pdb']
+__all__ = ['remote', 'config_console', 'config_history', 'enable_pdb']
 
-_DEFAULT_ENDPOINT_PATH = "/_ah/remote_api"
 
-def connect_to_app(app_id, user=None, password=None, path=None, address=None):
-  '''Attaches to the app_id instance at address (default: <app_id>.appspot.com).
+DEFAULT_ENDPOINT_PATH = "/_ah/remote_api"
+DEFAULT_ENDPOINT_HOST = 'localhost:8080'
 
-    :param app_id: Required. the app_id of the application to connect to.
-    :param user: if you don't specify a user you will be prompted.
-    :param password: if you don't specify a password you will be prompted.
-    :param path: path to remote_api handler. defaults to: /_ah/remote_api
-    :param address: defaults to: <app_id>.appspot.com
+
+def remote(app_id, hostname, path, email=None, password=None):
   '''
-  _path = path or _DEFAULT_ENDPOINT_PATH
-  if not address:
-    address = '{}.appspot.com'.format(app_id)
-  _init_remote_api(app_id, _path, address, user, password)
-
-def _auth_input():
-  '''Function to prompt user for credentials.'''
-  from getpass import getpass
-  return raw_input("Email: "), getpass("Password: ")
-
-def _init_remote_api(app_id, path, address, user=None, password=None):
-  '''Generic wrapper to initialize the remote_api_stub for a given path.
+  Attaches to the app_id instance at address (default: <app_id>.appspot.com).
 
     :param app_id: Required. the app_id of the application to connect to.
-    :param user: if you don't specify a user you will be prompted.
-    :param password: if you don't specify a password you will be prompted.
-    :param path: the path to the remote_api handler ex: /_ah/remote_api.
-    :param address: server to connect to, ex: <app_id>.appspot.com.
+    :param path: path to remote_api handler. defaults to: /_ah/remote_api
+    :param hostname: defaults to: localhost:8080
+    :param email: if you don't specify an email, you will be prompted.
+    :param password: if you don't specify a password, you will be prompted.
 
-  if user and password are not specified you will be prompted on
-  connection, if needed.'''
-  _auth = _auth_input
-  if user and password:
-    def _auth():
-      return (user, password)
-  remote_api_stub.ConfigureRemoteApi(app_id, path, _auth, address)
-  remote_api_stub.MaybeInvokeAuthentication()
+  if email and password are not specified you will be prompted on
+  connection, if needed.
+
+  NOTE: https://code.google.com/p/googleappengine/issues/detail?id=3258
+  remote_api does not work with Federated Login/OpenID. in order to use with
+  a custom domain, you must set the login method to Google Accounts API.
+  '''
+  if not hostname:
+    address = DEFAULT_ENDPOINT_HOST
+
+  def _auth_input():
+    '''Prompts user for credentials.'''
+    from getpass import getpass
+    return raw_input("Email: "), getpass("Password: ")
+
+  def _auth():
+    def _raw(value, *a):
+      '''Monkeypatches stdin.'''
+      from StringIO import StringIO
+      sys.stdin = StringIO(value)
+      return ''
+    if email and password:
+      return (_raw(email), _raw(password))
+    elif address == DEFAULT_ENDPOINT_HOST:
+      return (_raw(''), _raw(''))
+    else:
+      return _auth_input()
+
+  from google.appengine.tools import appengine_rpc
+  from google.appengine.ext.remote_api import remote_api_stub
+  remote_api_stub.ConfigureRemoteApi(
+      app_id, path, _auth, hostname,
+      rpc_server_factory=appengine_rpc.HttpRpcServer)
+
+  # remote_api_stub.MaybeInvokeAuthentication()
   environ["SERVER_SOFTWARE"] = "Development (remote_api)/1"
 
 
@@ -64,14 +75,20 @@ def _init_remote_api(app_id, path, address, user=None, password=None):
 # --------------------------------------------------------------------------- #
 
 def config_console():
-  '''Enables tab completion.
-  # from http://www.doughellmann.com/PyMOTW/rlcompleter/index.html'''
+  '''
+  Enables tab completion.
+
+  see: http://www.doughellmann.com/PyMOTW/rlcompleter/index.html
+  '''
   import rlcompleter, readline
   readline.parse_and_bind('bind ^I rl_complete')
 
 def config_history():
-  '''Configures bash history.
-  see: http://dotfiles.org/~remote/.pythonrc.py'''
+  '''
+  Configures bash history.
+
+  see: http://dotfiles.org/~remote/.pythonrc.py
+  '''
   import atexit, readline
   histfile = path.join(environ["HOME"], ".remoteapi_history")
   try:
@@ -82,9 +99,12 @@ def config_history():
   del histfile
 
 def enable_pdb():
-  '''Enables interactive pdb debugging.'''
-  def info(type, value, tb):
+  '''
+  Enables interactive debugging via python's pdb module.
+  '''
+  def debugger(type, value, tb):
     import traceback, pdb
     traceback.print_exception(type, value, tb)
     pdb.pm()
-  sys.excepthook = info
+  sys.excepthook = debugger
+
